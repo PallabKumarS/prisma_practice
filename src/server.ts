bd165da33561bff397a29d22421e0e6e188f6ec7
PallabKumarS <pallabkumar26@gmail.com>
2025-04-08 20:30:41 +0600

    practicing prisma

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+//   const result = await prisma.post.create({
+//     data: {
+//       title: "Test",
+//       content: "Testing",
+//       published: true,
+//       author: "PKS",
+//     },
+//   });
+
+//   console.log(result);
+};
+
+main();
